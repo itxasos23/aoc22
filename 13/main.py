af20031a4bfcb2b98a393f0835a7da27d3703677
Itxaso Aizpurua <itsaso23a@gmail.com>
2022-12-13 07:50:57 +0100

    black 13/main.py

--- 13/main.py
+++ 13/main.py
@@ -1,9 +1,11 @@
-from typing import List, Tuple, Union
+from typing import List, Union
 from dataclasses import dataclass
+
 
 @dataclass
 class Message:
     contents: List
+
     def __lt__(self, other):
         return compare_elements(self.contents, other.contents)
 
@@ -34,10 +36,10 @@ def find_matching_delimiter(string):
     matching_delimiter_position = -1
     depth = 1
     for idx, element in enumerate(string[1:]):
-        depth += 1 if element == '[' else (-1 if element == ']' else 0)
+        depth += 1 if element == "[" else (-1 if element == "]" else 0)
 
         if depth == 0:
-            matching_delimiter_position = idx+1
+            matching_delimiter_position = idx + 1
             break
 
     if matching_delimiter_position == -1:
@@ -54,8 +56,8 @@ def parse_next_item(string: str) -> Union[List, int]:
     while not done and string:
         if string.startswith("["):
             matching_delimiter_position = find_matching_delimiter(string)
-            items.append(parse_next_item(string[:matching_delimiter_position+1]))
-            string = string[matching_delimiter_position+2:]
+            items.append(parse_next_item(string[: matching_delimiter_position + 1]))
+            string = string[matching_delimiter_position + 2 :]
 
         else:
             pos = string.find(",")
@@ -64,7 +66,7 @@ def parse_next_item(string: str) -> Union[List, int]:
                 done = True
             else:
                 items.append(int(string[:pos].strip()))
-                string = string[pos+1:].strip()
+                string = string[pos + 1 :].strip()
 
     return items
 
@@ -72,16 +74,22 @@ def parse_next_item(string: str) -> Union[List, int]:
 class AOC2213:
     def __init__(self, filename):
         data = open(filename).read().strip()
-        self.pairs = [list(map(parse_next_item, pair.split('\n')))for pair in data.split('\n\n')]
+        self.pairs = [
+            list(map(parse_next_item, pair.split("\n"))) for pair in data.split("\n\n")
+        ]
 
         data = data.replace("\n\n", "\n")
-        self.messages = [Message(contents=parse_next_item(line)) for line in data.splitlines()]
+        self.messages = [
+            Message(contents=parse_next_item(line)) for line in data.splitlines()
+        ]
 
     def test(self, pair):
         return compare_elements(pair[0], pair[1])
 
     def part_1(self):
-        return sum((pair_idx+1) * self.test(pair) for pair_idx, pair in enumerate(self.pairs))
+        return sum(
+            (pair_idx + 1) * self.test(pair) for pair_idx, pair in enumerate(self.pairs)
+        )
 
     def part_2(self):
         self.messages.extend([Message(contents=[[2]]), Message(contents=[[6]])])
@@ -90,19 +98,19 @@ class AOC2213:
         result = 1
         for (idx, message) in enumerate(self.messages):
             if message.contents in ([[2]], [[6]]):
-                result *= idx +1
+                result *= idx + 1
         return result
 
 
 if __name__ == "__main__":
     aoc = AOC2213("input_ex.txt")
-    print(f'Part 1: {aoc.part_1()}')
+    print(f"Part 1: {aoc.part_1()}")
 
     aoc = AOC2213("input_ex.txt")
-    print(f'Part 2: {aoc.part_2()}')
+    print(f"Part 2: {aoc.part_2()}")
 
     aoc = AOC2213("input.txt")
-    print(f'Part 1: {aoc.part_1()}')
+    print(f"Part 1: {aoc.part_1()}")
 
     aoc = AOC2213("input.txt")
-    print(f'Part 2: {aoc.part_2()}')
+    print(f"Part 2: {aoc.part_2()}")
